f9de5d2a4d931afc9fc2753af46c9f5aeac600a0
hirokoymj <hiroko@hirokoymj.com>
2025-05-20 14:17:15 -0500

    add: render a nested data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Counter } from "./features/counter/Counter";
 import { Todo } from "./features/todo/Todo";
+import { DemoView } from "./features/demo/Demo";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>Todo Redux Demo</h2>
+      <h2>Redux Demo</h2>
       <Todo />
+      <hr />
       <h2>Counter Redux Demo</h2>
       <Counter />
+      <hr />
+      <h2>Nested render demo</h2>
+      <DemoView />
     </div>
   );
 }
